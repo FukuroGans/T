9f9e5514ba953266a5b0034f33bd8cd3bcfd68e8
FukuroGans <51269240+FukuroGans@users.noreply.github.com>
2019-08-27 17:50:27 +0700

    Add files via upload

---
+++ o.sh
@@ -0,0 +1,10 @@
+purple='\033[34;1m'
+green='\033[32;1m'  
+pink='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+read -p "Masukan Folder : " folder
+mpv /sdcard/$folder
